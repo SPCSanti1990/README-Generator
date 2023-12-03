28600ed4d8ee400ffd6b4db7b0ac1dee7b0fb268
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-12-03 16:07:18 -0500

    questions added, init function added, write file function added

--- Develop/index.js
+++ Develop/index.js
@@ -8,75 +8,114 @@ const questions = [{
     type: 'input',
     name: 'github',
     message: 'What is your GitHub Username?',
+    validate: your_github => {
+        if (your_github) {
+            return true;
+        }
+        else {
+            console.log("Enter your GitHub username");
+            return false;
+        }
+    }
   },
   {
     type: 'input',
     name: 'email',
     message: 'What is your email address?',
+    validate: your_email => {
+        if (your_email) {
+            return true;
+        }
+        else {
+            console.log("Enter your email address");
+            return false;
+        }
+    }
   },
   {
     type: 'input',
     name: 'projectName',
     message: 'What is the project name?',
+    validate: your_proj => {
+        if (your_proj) {
+            return true;
+        }
+        else {
+            console.log("Enter a title for your project");
+            return false;
+        }
+    }
   },
   {
     type: 'input',
     name: 'projectDesc',
     message: 'Please write a short desctiption of your project:',
+    validate: your_input => {
+        if (your_input) {
+            return true;
+        }
+        else {
+            console.log("Please provide a description of your project");
+            return false;
+        }
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Choose a license that will be used in your project:',
+    choices: ['MIT', 'GNU', 'Apache', 'None of the above'],
+    validate: your_license => {
+        if (your_license) {
+            return true;
+        }
+        else {
+            console.log("Enter a title for your project");
+            return false;
+        }
+    }
   },
   {
     type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username',
+    name: 'command',
+    message: 'What command should be run to install dependencies?',
+  },
+  {
+    type: 'input',
+    name: 'run',
+    message: 'What command should be run to run tests?',
+  },
+  {
+    type: 'input',
+    name: 'repo',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contact',
+    message: 'What does the user need to know about contributing to repo?',
   }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+
+    console.log("Success! Your README file has been created");
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
-
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
-    );
-  });
