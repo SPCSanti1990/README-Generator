f077a5c7c29cf43dccec4c47c9554079df19f12a
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-12-03 16:50:17 -0500

    gerateMarkdown file edited

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,28 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## License:
+  [![license](https://img.shields.io/badge/license-${data.license}-blue.svg)]
+  ## Table of Contents
+  - [Description](#descritpion)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Additional Info](#additional-info)
 
+  ## Description:
+  ${data.description}
+  ## Intallation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.license}
+  ## Contribution:
+  ${data.contribution}
+  ## Contact Information:
+  - GitHub: [${data.github}](https://github.com/${data.github})
+  - Email: [${data.email}]
 `;
 }
 
